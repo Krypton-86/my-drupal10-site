83a3d3a78087028dcbd437b388621bf85792c608
Petro Mykolaichuk <petro.mykolaichuk@smile-ukraine.com>
2023-05-17 17:23:52 +0300

    Added movie listing

--- web/modules/custom/movie_directory/src/Controller/MovieListing.php
+++ web/modules/custom/movie_directory/src/Controller/MovieListing.php
@@ -7,12 +7,47 @@ use Drupal\Core\Controller\ControllerBase;
 class MovieListing extends ControllerBase {
 
   public function view () {
+    $this->listMovies();
     $content = [];
-    $content['name'] = 'BEN';
+    $content['movies'] = $this->createMovieCard();
 
     return [
       '#theme' => 'movie-listing',
       '#content' => $content,
     ];
+  }
+
+  public function listMovies() {
+    /** @var \MovieAPIConnector $movie_api_connector_service */
+    $movie_api_connector_service = \Drupal::service('movie_directory.api_connector');
+    $movie_list = $movie_api_connector_service->discoverMovies();
+    if (!empty($movie_list->results)) {
+      return $movie_list->results;
+    }
+    return [];
+  }
+
+  public function  createMovieCard() {
+    /** @var \Drupal\movie_directory\MovieAPIConnector $movie_api_connector_service */
+    $movie_api_connector_service = \Drupal::service('movie_directory.api_connector');
+    $movieCards = [];
+
+    $movies = $this->listMovies();
+
+    if (!empty($movies)) {
+      foreach ($movies as $movie) {
+        $content = [
+          'title' => $movie->title,
+          'description' => $movie->overview,
+          'movie_id' => $movie->id,
+          'image' => $movie_api_connector_service->getImageUrl($movie->poster_path)
+        ];
+        $movieCards[] = [
+          '#theme' => 'movie-card',
+          '#content' => $content
+        ];
+      }
+    }
+    return $movieCards;
   }
 }
